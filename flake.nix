5167ab67b9ce31f2ef924432eca8ee02744c2e18
waalge <waalgy@gmail.com>
2023-06-22 19:42:49 +0000

    builds

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
         inherit (nixpkgs) lib;
 
         # see flake `variants` below for alternative compilers
-        defaultCompiler = "ghc928";
+        defaultCompiler = "ghc927";
         # We use cabalProject' to ensure we don't build the plan for
         # all systems.
         cabalProject = nixpkgs.haskell-nix.cabalProject' ({config, ...}: {
